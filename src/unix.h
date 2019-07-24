2302c94e4da10ee086a25f2e5b2f25e5e6aee887
Thomas Spader <thomas.spader@keysight.com>
2019-07-23 23:20:56 -0400

    platform header files

---
+++ src/unix.h
@@ -0,0 +1,2 @@
+#include <unistd.h>
+static const char* conf = "/Users/thspader/Programming/tdeditor/src/tded.conf";
---
+++ src/win32.h
@@ -0,0 +1,4 @@
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+
+static const char* conf = "C:/Programming/tdeditor/src/tded.conf";
